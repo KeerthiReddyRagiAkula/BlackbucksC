e74ba737bc83ed1e4b1cdda029c15abd59fb8696
Keerthi <139979610+KeerthiReddyRagiAkula@users.noreply.github.com>
2023-08-31 11:16:47 +0530

    Add files via upload

---
+++ SingleLinkedlist.c
@@ -0,0 +1,168 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<stdlib.h>
+void addFirst();
+void display();
+struct node{
+    int info;
+    struct node *ptr;
+};
+typedef struct node node;
+node *head=NULL,*tail=NULL,*temp;
+node* createNode();
+node* createNode(){
+    temp=(node *) malloc(sizeof(node));
+    if(temp==NULL){
+        printf("Node is not Created");
+        return;
+    }
+    printf("Enter data:");
+    scanf("%d",&temp->info);
+    temp->ptr=NULL;
+    return temp;
+}
+
+main()
+{
+    int ch;
+    while(10){
+        printf("\nEnter \n 1 for addFirst \n 2 for addLast \n 3 for insertmiddle \n 4 for display \n 5 for deletefirst \n 6 fro deletelast\n 7 for size\n 8 for exit ");
+        scanf("%d",&ch);
+   
+       switch(ch){
+           case 1:addFirst(); break;
+           case 2:addLast(); break;
+           case 3:insertMiddle(); break;
+           case 4:display(); break;
+           case 5:deleteFirst(); break;
+           case 6:deleteLast(); break;
+           case 7:sizeoflist(); break;
+           case 8:exit(10); 
+           
+       }
+   } 
+    
+}
+void addFirst(){ //node * addFirst(){ return head;
+
+    node *t;
+    t=createNode();
+    if(head==NULL){
+        head=t;
+        tail=t;
+    }
+    else{
+        t->ptr=head;
+        head=t;
+    }
+}
+void display(){
+    temp=head;
+    printf("%u",temp);
+    while(temp!=NULL){
+        printf("-> || %d - %u", temp->info, temp->ptr);
+        temp=temp->ptr;
+    }
+}
+void addLast(){  //node *addLast(){ return tail
+    node *t;
+    if(head==NULL){
+        head=createNode();
+        tail=head;
+    }
+    t=createNode();
+    tail->ptr=t;
+    tail=t;
+}
+void deleteFirst(){
+    if(head==NULL){
+        printf("\n\nList is empty");
+        return;
+    }
+    if(head==tail){
+        head=tail=NULL;
+    }
+    else{
+        head=head->ptr;
+    }
+}
+
+void deleteLast(){
+node *prev;
+node *th;
+    if(head==NULL){
+        printf("List is Empty");
+    }
+    else if(head==tail){
+        head=tail=NULL;
+    }
+    else{
+        th=head;
+        if(th->ptr!=NULL){
+            prev=th;
+            th=th->ptr;
+        }
+        prev->ptr=NULL;
+        tail=prev;
+    }
+}
+
+int sizeoflist(){
+    node *th=head;
+    int count=0;
+    while(th!=NULL){
+        count++;
+        th=th->ptr;
+    }
+    
+    return count;
+}
+void insertMiddle(){
+    int pos,i=0;
+    node *tins;
+    node *temp;
+ 
+    node *th=head;
+    int count=sizeoflist();
+    printf("Enter the position of the element you want to insert:  ");
+    scanf("%d",&pos);
+    if(pos<0)
+    {
+        addFirst();
+        
+        return;
+    }
+    else if(pos>count){
+        addLast();
+        return;
+    }
+
+       tins=createNode();
+        while(pos>1){
+            temp=th;
+            th=th->ptr;
+        }
+            tins->ptr=th->ptr;
+            temp->ptr=tins;
+            
+        
+    
+    printf("%d is inserted", tins->info);
+    
+    
+}
+
+
+
+
+
+
+
+
